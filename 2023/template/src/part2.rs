2413904cb11221933700b88fec5232b8efe0baa3
Sahana Tankala <sahanatankala@gmail.com>
2023-12-02 15:22:19 -0500

    lots of stuff

---
+++ 2023/template/src/part2.rs
@@ -0,0 +1,16 @@
+use crate::aoc_error::AocError;
+
+pub fn process(input: &str) -> anyhow::Result<String, AocError> {
+    todo!("{{project-name}} part2 unimplemented!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+
+    #[test]
+    fn test_process() -> anyhow::Result<()> {
+        todo!("{{project-name}} part1 tests unimplemented!");
+    }
+}
