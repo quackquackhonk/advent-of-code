6d70bc9b79fdcaeb11de3e2351fe791f087194ee
Sahana Tankala <sahanatankala@gmail.com>
2023-12-03 14:22:05 -0500

    Finished!

--- 2023/day-3/src/lib.rs
+++ 2023/day-3/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod aoc_error;
 pub mod part1;
 pub mod part2;
+pub mod gear;
--- 2023/day-3/src/part2.rs
+++ 2023/day-3/src/part2.rs
@@ -1,10 +1,77 @@
-pub fn process(_input: &str) -> anyhow::Result<usize> {
-    todo!("day-3 part2 unimplemented!");
+use std::collections::HashMap;
+
+use itertools::Itertools;
+use regex::{Match, Regex};
+
+use crate::gear::Gear;
+
+pub type Index = (usize, usize);
+
+pub fn process(input: &str) -> anyhow::Result<usize> {
+    let lines: Vec<&str> = input.lines().collect();
+    let num_map = create_number_map(&lines);
+
+    let gear_centers = create_gear_centers(&lines);
+
+    let o: usize = create_gears_list(gear_centers, num_map).iter().filter_map(|gear| {
+        if gear.valid() {
+            Some(gear.gear_ratio())
+        }
+        else {
+            None
+        }
+    }).sum();
+
+    Ok(o)
+}
+
+pub fn create_gears_list(centers: Vec<Index>, num_map: HashMap<Index, usize>) -> Vec<Gear> {
+    centers.iter().map(|center_idx| Gear::from_center_and_nums(center_idx, &num_map)).collect()
+}
+
+pub fn create_gear_centers(lines: &[&str]) -> Vec<Index> {
+    lines
+        .iter()
+        .enumerate()
+        .flat_map(|(line_idx, line)| {
+            line.chars()
+                .enumerate()
+                .filter_map(move |(char_idx, char)| {
+                    if char == '*' {
+                        Some((line_idx, char_idx))
+                    } else {
+                        None
+                    }
+                })
+        })
+        .collect()
+}
+
+pub fn create_number_map(lines: &[&str]) -> HashMap<Index, usize> {
+    lines
+        .iter()
+        .enumerate()
+        .flat_map(|(line_idx, line)| {
+            let re = Regex::new(r"\d+").unwrap();
+            re.find_iter(line)
+                .map(|m| (line_idx, m))
+                .collect::<Vec<(usize, Match)>>()
+        })
+        .fold(HashMap::new(), |mut acc, (line_idx, m)| {
+            let number = m
+                .as_str()
+                .parse::<usize>()
+                .expect("This will always be a number");
+            for digit_idx in m.range() {
+                acc.insert((line_idx, digit_idx), number);
+            }
+            acc
+        })
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::part2::process;
+    use crate::part2::{process, create_number_map, create_gear_centers, create_gears_list};
 
     #[test]
     fn test_process() -> anyhow::Result<()> {
@@ -18,6 +85,11 @@ mod tests {
 ......755.
 ...$.*....
 .664.598.. ";
+
+        let lines: Vec<&str> = input.lines().collect();
+        let num_map = create_number_map(&lines);
+        let centers = create_gear_centers(&lines);
+        dbg!(create_gears_list(centers, num_map));
 
         assert_eq!(467835, process(input).expect("Should give us a number"));
         Ok(())
