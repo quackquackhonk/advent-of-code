46ba9eee1f244b901c00cf37225d51ca9dc6dd08
Sahana Tankala <sahanatankala@gmail.com>
2023-12-03 12:26:15 -0500

    gaming

--- 2023/day-3/src/part2.rs
+++ 2023/day-3/src/part2.rs
@@ -1,16 +1,25 @@
-use crate::aoc_error::AocError;
-
-pub fn process(_input: &str) -> anyhow::Result<String, AocError> {
+pub fn process(_input: &str) -> anyhow::Result<usize> {
     todo!("day-3 part2 unimplemented!");
 }
 
 #[cfg(test)]
 mod tests {
-    
-
+    use crate::part2::process;
 
     #[test]
     fn test_process() -> anyhow::Result<()> {
-        todo!("day-3 part1 tests unimplemented!");
+        let input = "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598.. ";
+
+        assert_eq!(467835, process(input).expect("Should give us a number"));
+        Ok(())
     }
 }
