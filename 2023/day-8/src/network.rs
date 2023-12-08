0dde0ffd8053f7d3c1f538a428eca87aecff627a
Sahana Tankala <sahanatankala@gmail.com>
2023-12-08 15:49:40 -0500

    part2

--- 2023/day-8/src/network.rs
+++ 2023/day-8/src/network.rs
@@ -1,15 +1,21 @@
 use std::collections::HashMap;
 
+use itertools::Itertools;
+
 #[derive(Debug, PartialEq)]
 pub struct Map {
     pub directions: Vec<Direction>,
     pub edges: HashMap<String, Branch>,
-    pub start: String
+    pub start: String,
 }
 
 impl Map {
     pub fn new(directions: Vec<Direction>, start: String, edges: HashMap<String, Branch>) -> Self {
-        Self { directions, edges, start }
+        Self {
+            directions,
+            edges,
+            start,
+        }
     }
 
     pub fn go_to(&self, dest: &str) -> usize {
@@ -23,10 +29,74 @@ impl Map {
             count += 1;
 
             if curr == dest {
-                break
+                break;
             }
         }
         count
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct GhostMap {
+    pub directions: Vec<Direction>,
+    pub edges: HashMap<String, Branch>,
+    pub starts: Vec<String>,
+    pub dests: Vec<String>,
+}
+
+impl GhostMap {
+    pub fn new(
+        directions: Vec<Direction>,
+        edges: HashMap<String, Branch>,
+        starts: Vec<String>,
+        dests: Vec<String>,
+    ) -> Self {
+        Self {
+            directions,
+            edges,
+            starts,
+            dests,
+        }
+    }
+
+    pub fn go_to(self) -> i64 {
+        self.starts
+            .iter()
+            .map(|st| {
+                let mut curr = st;
+                let mut count: i64 = 0;
+                for dir in self.directions.iter().cycle() {
+                    let Some(branch) = self.edges.get(curr) else {
+                        unreachable!("All nodes can be reached!");
+                    };
+
+                    curr = branch.go(dir);
+                    count += 1;
+                    if curr.ends_with('Z') {
+                        break;
+                    }
+                }
+
+                count
+            })
+            .fold(1, num_integer::lcm)
+    }
+}
+
+impl From<Map> for GhostMap {
+    fn from(value: Map) -> Self {
+        let mut starts: Vec<String> = Vec::new();
+        let mut dests: Vec<String> = Vec::new();
+        for node in value.edges.keys() {
+            if node.ends_with("A") {
+                starts.push(node.to_string())
+            }
+            if node.ends_with("Z") {
+                dests.push(node.to_string());
+            }
+        }
+
+        GhostMap::new(value.directions, value.edges, starts, dests)
     }
 }
 
@@ -60,7 +130,7 @@ impl From<char> for Direction {
         match value {
             'L' => Self::Left,
             'R' => Self::Right,
-            _ => unreachable!("Got {}", value)
+            _ => unreachable!("Got {}", value),
         }
     }
 }
--- 2023/day-8/src/part2.rs
+++ 2023/day-8/src/part2.rs
@@ -1,5 +1,9 @@
-pub fn process(input: &str) -> anyhow::Result<usize> {
-    todo!("day-8 part2 unimplemented!");
+use crate::network::GhostMap;
+use crate::parse::parse_map;
+
+pub fn process(input: &str) -> anyhow::Result<i64> {
+    let (_, map) = parse_map(input).expect("This should parse!");
+    Ok(GhostMap::from(map).go_to())
 }
 
 #[cfg(test)]
@@ -9,8 +13,17 @@ mod tests {
 
     #[test]
     fn test_process() -> anyhow::Result<()> {
-        let input = "";
-        assert_eq!(0, process(input)?);
+        let input = "LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)";
+        assert_eq!(6, process(input)?);
         Ok(())
     }
 }
